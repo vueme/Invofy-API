d9fa7cd65e9f36607d986b33e9bacce18a3dd5b0
Maciej Siwek <ms223iu@student.lnu.se>
2018-03-19 16:52:33 +0100

    [IMPROV] General post-launch structural changes (#1)
    * Updated
    
    Changes to GIT structure
    
    * Changed default server IP
    
    Now defaults to 127.0.0.1. Can be specified using IP system variable
    
    * Added info about this API on root GET request
    
    * Updated readme

---
+++ generic/GenericController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * [PUBLIC]
+ * Returns generic information about this API
+ */
+router.get('/', function (req, res) {
+  return res.status(200).send("Just an API created by Maciej Siwek. You can find more information about it on https://github.com/ms223iu/Invofy-API.");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
+const ip = process.env.IP || '127.0.0.1';
 const port = process.env.PORT || 3000;
-const server = app.listen(port, function () {
-  console.log('ExpressJS listening on port ' + port);
+
+const server = app.listen(port, ip, function () {
+  console.log('ExpressJS listening on ' + ip + ':' + port + ' in ' + process.env.NODE_ENV + ' mode');
 });
 
 module.exports = app;
