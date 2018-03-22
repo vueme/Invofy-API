f0b569a1349642720db6757ba97bbc1121e0ddea
Maciej Siwek <ms223iu@student.lnu.se>
2018-03-23 00:59:21 +0100

    Email format is now validated. Adjusted msgs

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -16,23 +16,27 @@ const User = require('../user/User');
  * @return JWT Authorization token
  */
 router.post('/', function (req, res) {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   // No email or password specified
   if (!req.body.email && !req.body.password) {
-    return res.status(400).json({ 'error': 'Email or username field was not specified' });
+    return res.status(400).json({ 'error': 'Email or username was not specified' });
   }
+
+  // Check if email is in valid format
+  if (!emailRegex.test(req.body.email)) return res.status(400).json({ 'error': 'Email you specified is not valid' });
 
   // Look for user with a specific email in database
   User.findOne({ email: req.body.email }, 'password active', function (err, user) {
-    const incorrectCredentials = { 'error': 'Login failed. Incorrect email or password' };
 
     // No user with provided email was found
-    if (!user) return res.status(404).json(incorrectCredentials);
+    if (!user) return res.status(404).json({ 'error': 'Account with that email does not exist' });
 
     // Compare password from the database with the one that was sent by user
     bcrypt.compare(req.body.password, user.password, function (err, result) {
 
       // Password was incorrect
-      if (!result) return res.status(403).json(incorrectCredentials);
+      if (!result) return res.status(403).json({ 'error': 'Incorrect email or password' });
 
       // User account is not activated or was suspsneded
       if (!user.active) return res.status(401).json({ 'error': 'Your account is not yet activated or was suspended. Contact support for more information' });
